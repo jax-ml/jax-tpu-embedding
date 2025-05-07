4e7bf04f99ed0cd75dd01b934f2ced00fe4cc5c5
Aditya Gupta <manuadg@google.com>
2025-05-07 11:49:52 -0700

    Change heuristic based parameter `static_buffer_size_multiplier` to FDO tunable parameter `feature_spec.table_spec.stacked_table_spec.suggested_coo_buffer_size`.
    PiperOrigin-RevId: 755944796

--- jax_tpu_embedding/sparsecore/lib/nn/embedding_spec.py
+++ jax_tpu_embedding/sparsecore/lib/nn/embedding_spec.py
@@ -351,6 +351,7 @@ class TableSpec:
   combiner: str
   max_ids_per_partition: int = 256
   max_unique_ids_per_partition: int = 256
+  suggested_coo_buffer_size: int | None = None
   # This points to the stacked table spec which this table belongs to.
   # If this is None, this table is the top-most table.
   stacked_table_spec: StackedTableSpec | None = dataclasses.field(
@@ -424,3 +425,4 @@ class StackedTableSpec:
   total_sample_count: int
   max_ids_per_partition: int = 256
   max_unique_ids_per_partition: int = 256
+  suggested_coo_buffer_size: int | None = None
