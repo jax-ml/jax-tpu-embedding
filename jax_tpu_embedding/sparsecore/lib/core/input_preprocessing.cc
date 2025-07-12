bdd762c0c02d5161ee528f69af8399048e65e3d4
Aditya Gupta <manuadg@google.com>
2025-07-11 17:23:29 -0700

    [JAX SC] Reduce the log message for sparse core to log every 10001 times.
    PiperOrigin-RevId: 782163980

--- jax_tpu_embedding/sparsecore/lib/core/input_preprocessing.cc
+++ jax_tpu_embedding/sparsecore/lib/core/input_preprocessing.cc
@@ -161,7 +161,8 @@ void CheckBufferUsage(int max_required_buffer_size_per_device,
     const int64_t buffer_shortfall =
         std::max(int64_t{0}, required_buffer_bytes - coo_buffer_bytes);
 
-    LOG(WARNING) << absl::StrFormat(
+    static constexpr int kLargePrime = 10001;
+    LOG_EVERY_N(WARNING, kLargePrime) << absl::StrFormat(
         "Required usage %.2f%% (%d bytes) of computed/given buffer size "
         "(%d bytes) for stacked table %s (Wasted space: %d bytes, "
         "Buffer shortfall: %d bytes)",
