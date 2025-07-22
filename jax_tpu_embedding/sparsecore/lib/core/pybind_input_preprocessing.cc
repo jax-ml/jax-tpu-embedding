134ffca6f246f8061053fe680d69c111f7ccd80c
Aditya Gupta <manuadg@google.com>
2025-07-22 12:58:13 -0700

    [JAX SC]  Make optional arguments keyword-only.
    PiperOrigin-RevId: 785964038

--- jax_tpu_embedding/sparsecore/lib/core/pybind_input_preprocessing.cc
+++ jax_tpu_embedding/sparsecore/lib/core/pybind_input_preprocessing.cc
@@ -211,29 +211,27 @@ PYBIND11_MODULE(pybind_input_preprocessing, m) {
       .value("SPLIT_THEN_STACK", FeatureStackingStrategy::kSplitThenStack)
       .export_values();
   m.def("PreprocessSparseDenseMatmulInput",
-        &PyNumpyPreprocessSparseDenseMatmulInput, pybind11::arg("features"),
-        pybind11::arg("feature_weights"), pybind11::arg("feature_specs"),
-        pybind11::arg("local_device_count"),
-        pybind11::arg("global_device_count"),
-        pybind11::arg("num_sc_per_device"),
-        pybind11::arg("sharding_strategy") = ShardingStrategy::kMod,
-        pybind11::arg("has_leading_dimension") = false,
-        pybind11::arg("allow_id_dropping") = false,
-        pybind11::arg("feature_stacking_strategy") =
+        &PyNumpyPreprocessSparseDenseMatmulInput, py::arg("features"),
+        py::arg("feature_weights"), py::arg("feature_specs"),
+        py::arg("local_device_count"), py::arg("global_device_count"),
+        py::kw_only(), py::arg("num_sc_per_device"),
+        py::arg("sharding_strategy") = ShardingStrategy::kMod,
+        py::arg("has_leading_dimension") = false,
+        py::arg("allow_id_dropping") = false,
+        py::arg("feature_stacking_strategy") =
             FeatureStackingStrategy::kSplitThenStack,
-        pybind11::arg("batch_number") = 0);
+        py::arg("batch_number") = 0);
   m.def("PreprocessSparseDenseMatmulSparseCooInput",
-        &PySparseCooPreprocessSparseDenseMatmulInput, pybind11::arg("indices"),
-        pybind11::arg("values"), pybind11::arg("dense_shapes"),
-        pybind11::arg("feature_specs"), pybind11::arg("local_device_count"),
-        pybind11::arg("global_device_count"),
-        pybind11::arg("num_sc_per_device"),
-        pybind11::arg("sharding_strategy") = ShardingStrategy::kMod,
-        pybind11::arg("has_leading_dimension") = false,
-        pybind11::arg("allow_id_dropping") = false,
-        pybind11::arg("feature_stacking_strategy") =
+        &PySparseCooPreprocessSparseDenseMatmulInput, py::arg("indices"),
+        py::arg("values"), py::arg("dense_shapes"), py::arg("feature_specs"),
+        py::arg("local_device_count"), py::arg("global_device_count"),
+        py::kw_only(), py::arg("num_sc_per_device"),
+        py::arg("sharding_strategy") = ShardingStrategy::kMod,
+        py::arg("has_leading_dimension") = false,
+        py::arg("allow_id_dropping") = false,
+        py::arg("feature_stacking_strategy") =
             FeatureStackingStrategy::kSplitThenStack,
-        pybind11::arg("batch_number") = 0);
+        py::arg("batch_number") = 0);
   py::class_<SparseDenseMatmulInputStats>(m, "SparseDenseMatmulInputStats")
       .def(py::init<>())
       .def_readonly("max_ids_per_partition",
