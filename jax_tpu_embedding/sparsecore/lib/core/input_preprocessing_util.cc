338c8faf95c843efc0390b92602c88fc7f3f87b9
Ziyin Huang <ziyinh@google.com>
2025-10-09 17:47:06 -0700

    Rate-limit error logging for buffer overflow in input preprocessing.
    Changes `LOG(ERROR)` to `LOG_EVERY_N(ERROR, 100)` to reduce log spam when the static buffer size is insufficient, as this condition can occur frequently.
    
    PiperOrigin-RevId: 817406279

--- jax_tpu_embedding/sparsecore/lib/core/input_preprocessing_util.cc
+++ jax_tpu_embedding/sparsecore/lib/core/input_preprocessing_util.cc
@@ -69,16 +69,17 @@ struct BufferFillingOptions {
 bool ValidIndices(int row_index, int coo_offset, int processed,
                   const BufferFillingOptions& options) {
   if (row_index >= options.lhs_row_end || coo_offset >= options.coo_end) {
-    LOG(ERROR) << "The static buffer size might be too small for the current "
-                  "batch. IDs may be dropped! "
-               << "Stopping row pointer filling for local SparseCore ID "
-               << options.local_sc_id << " at row index: " << row_index
-               << " and coo offset: " << coo_offset
-               << " because it reached the end of the buffer. "
-               << "The buffer size is: " << options.coo_buffer_size
-               << ". The coo_end is: " << options.coo_end
-               << ". Total number of input COOs: " << options.coo_tensors.size()
-               << ", while currently processed only: " << processed;
+    LOG_EVERY_N(ERROR, 100)
+        << "The static buffer size might be too small for the current "
+           "batch. IDs may be dropped! "
+        << "Stopping row pointer filling for local SparseCore ID "
+        << options.local_sc_id << " at row index: " << row_index
+        << " and coo offset: " << coo_offset
+        << " because it reached the end of the buffer. "
+        << "The buffer size is: " << options.coo_buffer_size
+        << ". The coo_end is: " << options.coo_end
+        << ". Total number of input COOs: " << options.coo_tensors.size()
+        << ", while currently processed only: " << processed;
     return false;
   }
   return true;
