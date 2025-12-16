e54126caa2dd060eeb4096ac896bc45c3bd23bde
The JAX SC Authors <google-ml-automation@google.com>
2025-12-15 19:43:07 -0800

    [JAX SC] Update default num_sc_per_sorting_task to num_sc_per_device.
    The default number of SparseCores grouped into a single sorting task is now set to the number of SparseCores per device.
    
    PiperOrigin-RevId: 845033214

--- jax_tpu_embedding/sparsecore/lib/core/input_preprocessing_util.h
+++ jax_tpu_embedding/sparsecore/lib/core/input_preprocessing_util.h
@@ -398,11 +398,11 @@ struct PreprocessSparseDenseMatmulInputOptions {
       PreprocessingThreadPoolSchedule;
 
   // Number of SparseCores to group into a single sorting task.
-  // This controls the parallelism of the sorting phase. Defaults to 4, which
-  // our non-parallel case.
+  // This controls the parallelism of the sorting phase. Defaults to
+  // num_sc_per_device.
   // TODO(b/469153631): Figure out heuristic to avoid over-parallelization
   // of small tables (which incur significant overhead).
-  int num_sc_per_sorting_task = 4;
+  int num_sc_per_sorting_task = num_sc_per_device;
 
   // Returns the total number of SparseCores across all devices and hosts.
   uint32_t GetNumScs() const { return num_sc_per_device * global_device_count; }
