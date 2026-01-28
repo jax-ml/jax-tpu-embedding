733ee4c75e116977827a4f88f758a18b4445d7e0
Aditya Gupta <manuadg@google.com>
2026-01-28 09:10:21 -0800

    [JAX SC] Update module docstring for embedding.py.
    PiperOrigin-RevId: 862263203

--- jax_tpu_embedding/sparsecore/lib/nn/embedding.py
+++ jax_tpu_embedding/sparsecore/lib/nn/embedding.py
@@ -11,7 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""List of functions for embedding lookup."""
+"""Embedding layer API for SparseCore.
+
+This file contains functions for embedding lookup and update on SparseCore,
+including input preprocessing, forward/backward passes, and embedding table
+initialization and stacking.
+"""
 
 import collections
 import dataclasses
