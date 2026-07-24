7f3d46386435e08275c0cbff26f6b01c6721320c
Song Bai <songbaie@google.com>
2026-07-24 09:44:01 -0700

    small bug fix and adding test on 1d embedding_table input
    PiperOrigin-RevId: 953413305

--- jax_tpu_embedding/sparsecore/lib/core/primitives/tests/sparse_dense_matmul_optimizer_grad_test.py
+++ jax_tpu_embedding/sparsecore/lib/core/primitives/tests/sparse_dense_matmul_optimizer_grad_test.py
@@ -210,6 +210,72 @@ class SparseDenseMatmulGradWithOptimizerTest(absltest.TestCase):
     )
     np.testing.assert_allclose(updated_emb_table, expected_updated_emb_table)
 
+  def test_sc_emb_backward_pass_with_sgd_dim1(self):
+    mesh = jax.sharding.Mesh(self.global_devices, "x")
+    (
+        lhs_row_pointers,
+        lhs_local_embedding_ids,
+        lhs_local_sample_ids,
+        lhs_gains,
+    ) = input_preprocessing.preprocess_sparse_dense_matmul_input(
+        self.input_tensor,
+        self.input_weights,
+        mesh,
+        max_ids_per_partition=16,
+        max_unique_ids_per_partition=64,
+        num_sc_per_device=self.num_sc_per_device,
+    )
+
+    emb_table_dim1 = np.arange(self.vocab_size, dtype=np.float32) + 1.0
+    emb_table_sharded = utils.shard_emb_table(
+        emb_table_dim1,
+        num_devices=len(self.global_devices),
+        num_sc_per_device=4,
+    )
+
+    z_grad = jnp.full(
+        (self.batch_size // self.num_chips,),
+        0.01,
+        np.float32,
+    )
+    emb_tables = [emb_table_sharded[0]]
+    hyperparams = [0.01]
+
+    def sgd_jax(grad, table, lr):
+      return table - lr * grad
+
+    grad_aval = jax.ShapeDtypeStruct((1,), jnp.float32)
+    table_aval = jax.ShapeDtypeStruct((1,), jnp.float32)
+    lr_aval = jax.ShapeDtypeStruct((1,), jnp.float32)
+    closed_jaxpr = jax.make_jaxpr(sgd_jax)(grad_aval, table_aval, lr_aval)
+
+    (updated_emb_table,) = self.tpu_sparse_dense_matmul_grad_with_optimizer(
+        lhs_row_pointers,
+        lhs_local_embedding_ids,
+        lhs_local_sample_ids,
+        lhs_gains,
+        np.int32(1),
+        z_grad,
+        *hyperparams,
+        *emb_tables,
+        num_hyperparameters=len(hyperparams),
+        jaxpr=closed_jaxpr,
+        max_ids_per_partition=16,
+        max_unique_ids_per_partition=16,
+        computation_name="optimizer_test_computation_dim1",
+        sharding_strategy=1,
+    )
+
+    expected_updated_emb_table = self._get_expected_updated_table(
+        emb_table_dim1,
+        z_grad,
+        self.input_tensor,
+        self.input_weights.squeeze(),
+        sgd_jax,
+        hyperparams[0],
+    )
+    np.testing.assert_allclose(updated_emb_table, expected_updated_emb_table)
+
   def test_sc_emb_backward_pass_with_adagrad(self):
     # Process the input.
     mesh = jax.sharding.Mesh(self.global_devices, "x")
