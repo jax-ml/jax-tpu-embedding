ee12722c0419440d61f301b30f5b4fec80647543
Aditya Gupta <manuadg@google.com>
2026-09-16 11:49:56 -0700

    [JAX SC] Strongly type EmbeddingVariablesParam and PartitionedOptimizer
    * Parameterize EmbeddingVariablesParam with Mapping[str, EmbeddingVariables].
    * Parameterize PartitionedOptimizer with nnx.Module.
    
    PiperOrigin-RevId: 982637173

--- jax_tpu_embedding/sparsecore/lib/flax/nnx/embed.py
+++ jax_tpu_embedding/sparsecore/lib/flax/nnx/embed.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 """SparseCore embedding layer."""
 
+import collections.abc
 import functools
 from typing import override
 
@@ -27,7 +28,9 @@ import optax
 Nested = embedding.Nested
 
 
-class EmbeddingVariablesParam(nnx.Param):
+class EmbeddingVariablesParam(
+    nnx.Param[collections.abc.Mapping[str, embedding.EmbeddingVariables]]
+):
   """A Param subclass for embedding variables, used to filter model params."""
 
   pass
@@ -88,7 +91,7 @@ def get_named_sharding(
   return nnx.StateSharding(nnx.map_state(fn, nnx.state(node)))
 
 
-class PartitionedOptimizer(nnx.Optimizer):
+class PartitionedOptimizer(nnx.Optimizer[nnx.Module]):
   """A partitioned optimizer for models that use the SparseCoreEmbed layer.
 
   This is a wrapper around the nnx.Optimizer that splits the gradients into
